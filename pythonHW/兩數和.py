c3640962d825740e6ed2035d06b9c245c32b9537
huang01199 <151027106+huang01199@users.noreply.github.com>
2024-03-30 09:03:23 +0000

    modify

---
+++ pythonHW/兩數和.py
@@ -0,0 +1,12 @@
+a=(input("輸入任意長度串列，用逗號區隔:").split(","))
+num=[int(i) for i in a]
+b=int(input("請輸入目標數"))
+print("nums =",num,",","target =",b)
+x=0
+for i in range(len(num)):
+    for j in range(i+1,len(num)):
+        if num[i]+num[j]==b:
+            print([i,j])
+            x=x+1
+if(x==0):
+    print("not in list")    
