333090c7e95728461c70c75bedc61ebb90daf61e
huang01199 <151027106+huang01199@users.noreply.github.com>
2024-04-13 10:49:53 +0000

    modify

--- pythonHW/羅馬數字轉換.py
+++ pythonHW/羅馬數字轉換.py
@@ -1,11 +1,10 @@
 a=(input("輸入羅馬數字:"))
 b={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
 result=0
-c=0
 for i in range(len(a)-1):
     if b[a[i]]<b[a[i+1]]:
         result=result-b[a[i]]
     else:
         result=result+b[a[i]]
-result += b[a[-1]]
+result=result+b[a[-1]]
 print(result)
