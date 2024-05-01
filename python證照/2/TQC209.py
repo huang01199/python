adb712f153ec9569325d41afefed05931e120dd9
huang01199 <151027106+huang01199@users.noreply.github.com>
2024-05-01 18:01:05 +0000

    modify

---
+++ python證照/2/TQC209.py
@@ -0,0 +1,14 @@
+#請撰寫一程式，讓使用者輸入整數0或1，若輸入0則以橫排優先的方式輸出五五乘法表；輸入1則以直排優先的方式輸出五五乘法表；否則輸出「error」。
+a=eval(input())
+if a==0:
+    for i in range(1,5+1):
+        for j in range(1,5+1):
+            print(f"{i} x {j} = {i*j:<2d}",end="	")
+        print(" ")
+elif a==1:
+    for i in range(1,5+1):
+        for j in range(1,5+1):
+            print(f"{j} x {i} = {i*j:<2d}",end="	")
+        print(" ")
+else:       
+    print("error")
