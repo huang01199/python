d2ff55ebb8570ad2045cc5455e87150297b0c0da
huang01199 <151027106+huang01199@users.noreply.github.com>
2024-05-02 09:25:04 +0000

    modify

--- python證照/2/TQC209.py
+++ python證照/2/TQC209.py
@@ -3,12 +3,12 @@ a=eval(input())
 if a==0:
     for i in range(1,5+1):
         for j in range(1,5+1):
-            print(f"{i} x {j} = {i*j:<2d}",end="	")
-        print(" ")
+            print(f"{i} x {j} = {i*j}",end="\t")
+        print()
 elif a==1:
     for i in range(1,5+1):
         for j in range(1,5+1):
-            print(f"{j} x {i} = {i*j:<2d}",end="	")
-        print(" ")
+            print(f"{j} x {i} = {i*j}",end="\t")
+        print()
 else:       
     print("error")
