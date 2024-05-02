ee221be5032e0c7599414166b47489aeaa627b88
huang01199 <151027106+huang01199@users.noreply.github.com>
2024-05-02 09:59:13 +0000

    modify

--- python證照/2/TQC208.py
+++ python證照/2/TQC208.py
@@ -1,5 +1,6 @@
 #請撰寫一程式，讓使用者輸入一正整數，輸出小於此整數內的所有質數，質數後方請接一個半形空格。
 a=eval(input())
+
 for i in range(1,a):
     b=0
     for j in range(1,i+1):
@@ -7,4 +8,4 @@ for i in range(1,a):
             b=b+1
     if b==2:
         print(f"{i}",end=" ")
-    i=i+1
+    
