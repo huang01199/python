6194a4adf44396217373c30861853846ecd2825f
huang01199 <151027106+huang01199@users.noreply.github.com>
2024-05-06 03:41:44 +0000

    modify

--- python證照/1/TQC105.py
+++ python證照/1/TQC105.py
@@ -1,5 +1,4 @@
 #請撰寫一程式，讓使用者輸入兩個正整數，計算兩個正整數的總和後開根號（四捨五入至小數點後第二位）
 a=eval(input())
 b=eval(input())
-c=(a+b)**0.5
-print("result={:.2f}".format(c))
+print(f"result={(a+b)**0.5:.2f}")
--- python證照/1/TQC106.py
+++ python證照/1/TQC106.py
@@ -3,5 +3,4 @@ x1=eval(input())
 y1=eval(input())
 x2=eval(input())
 y2=eval(input())
-a=((x2-x1)**2+(y2-y1)**2)**0.5
-print("{:.2f}".format(a))
+print(f"{(((x2-x1)**2)+((y2-y1)**2))**0.5:.2f}")
