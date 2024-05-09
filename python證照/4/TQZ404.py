0a5600c25ba2544df3f872dd2c557a81d00da689
huang01199 <151027106+huang01199@users.noreply.github.com>
2024-05-09 10:02:51 +0000

    modify

--- python證照/4/TQZ404.py
+++ python證照/4/TQZ404.py
@@ -1,1 +1,16 @@
 #請撰寫一程式，讓使用者輸入一個長度不超過50字元的字串，此字串均為小寫字母，輸出該字串出現最多次的英文字母以及出現的次數。
+a=input()
+b={}
+for i in a:
+    if i not in b:
+        b[i]=1
+    else:
+        b[i]=b[i]+1
+
+for j,k in b.items():
+    if k==max(b.values()):
+        print(k)
+        print(j)
+
+        
+
